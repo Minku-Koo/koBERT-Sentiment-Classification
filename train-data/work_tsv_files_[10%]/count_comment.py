09771d2d17b389b7211fe559e47fe6ccdb1fd022
구민구 <corleone@kakao.com>
2021-07-01 01:40:59 +0900

    Create count_comment.py

---
+++ train-data/work_tsv_files_[10%]/count_comment.py
@@ -0,0 +1,27 @@
+
+# 총 댓글 몇개인지 확인
+
+def checkLine(filename):
+    with open("./"+filename+"-dict.tsv", "rt", encoding="utf-8") as f:
+        l = f.readlines()
+
+    return len(l)
+
+
+tlist = []
+for i in range(1,6): tlist.append(["before"+str(i),"after"+str(i)])
+
+result  = 0
+for period in tlist:
+    for t in period:
+        file = "daum"+t
+        count = checkLine(file)
+        result += count
+        print(file, ":", count)
+        
+        file = "naver"+t
+        count = checkLine(file)
+        result += count
+        print(file, ":", count)
+        
+print("-----\nAll >>", result)
